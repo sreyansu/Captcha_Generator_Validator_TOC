e78e31c67bc894e1ca997487e12a2ec1bdf98a95
sreyansu <sreyansu90@gmail.com>
2025-10-11 23:07:39 +0530

    updates

--- api/index.py
+++
@@ -1,5 +0,0 @@
-from server import app as flask_app
-from vercel_wsgi import make_lambda_handler
-
-# Vercel entry point: handler is discovered automatically
-handler = make_lambda_handler(flask_app)
